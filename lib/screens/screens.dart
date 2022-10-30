fbf859b99a2afb5594e1d5f83007bb3f71c021bf
VikingoK <kevin1201959@gmail.com>
2022-10-30 02:58:29 -0500

    eliminacion exportacion innecesaria

--- lib/screens/screens.dart
+++ lib/screens/screens.dart
@@ -1,3 +1,2 @@
-export 'package:fl_components/screens/listView1_screen.dart';
 export 'package:fl_components/screens/post_screen.dart';
 export 'package:fl_components/screens/comentarios_screen.dart';
